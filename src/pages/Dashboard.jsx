9f5e1e63e6a4da1b8969318bbaa81ff75c8c5df2
ReshmaMasoodha <reshmamasoodhadgl@gmail.com>
2026-03-03 16:58:50 +0530

    fix: correct dashboard balance calculation

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
 export default function Dashboard ({data}){
-  const total= data.reduce((accumlator,data)=> {return accumlator+data.amount},0)
-  
+  const totalincome= data.reduce((accumlator,data)=> {if (data.type == "income"){return accumlator+data.amount}
+    return accumlator;
+  },0)
+  const totalExpense = data.reduce((accumulator, currentValue)=>{
+    if (currentValue.type == "expense"){
+      return accumulator+currentValue.amount;
+    }
+    return accumulator;
+  },0);
+  const netBalance = totalincome - totalExpense;
   return(
     <div>
-   <p>Balance</p>{total}
+   <p>Balance</p>{netBalance}
    </div>
   )
 }
