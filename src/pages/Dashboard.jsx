a6e608836a345c0ad3b169c1ea7881911f67a9ec
ReshmaMasoodha <reshmamasoodhadgl@gmail.com>
2026-03-30 10:24:04 +0530

    add header and subtitle

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,10 @@
 export default function Dashboard ({data}){
   return(
     <div>
+      <h2 style ={{padding: '10px',
+marginBottom: '15px',
+gap: '10px'}}> Money Tracker</h2>
+    <p>Track your Income and Expenses</p>
    <p>Balance</p>{data.netBalance}
    </div>
   )
